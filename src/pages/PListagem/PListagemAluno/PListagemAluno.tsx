ac7748d6ed2a4a35aee341219d7f9a16b0d58f49
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-04-24 09:40:13 -0300

    style: ajustando responsividade Home
    Ajustando a estilização dos componentes e páginas principais para que estejam responsíveis a um maior número de telas.

--- src/pages/PListagem/PListagemAluno/PListagemAluno.tsx
+++ src/pages/PListagem/PListagemAluno/PListagemAluno.tsx
@@ -5,11 +5,11 @@ import Rodape from "../../../components/Rodape/Rodape";
 
 function PListagemAluno(): JSX.Element {
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <Navegacao />
             <ListagemAlunos />
             <Rodape />
-        </>
+        </div>
     );
 }
 
