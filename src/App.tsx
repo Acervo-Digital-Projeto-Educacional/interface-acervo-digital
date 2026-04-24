4bb71778f4b704d141f7f81f472789b356ded791
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-04-24 16:07:50 -0300

    feat: adicionada ação no botão novo
    Adicionada ação no botão novo que quando acionado leva o usuário até a página de cadastro da entidade.
    Foram criadas as rotas dos componentes no arquivo App.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,25 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import PHome from './pages/PHome/PHome'
 import PLogin from './pages/PLogin/PLogin'
+import ProtectedRoute from './components/Rotas/ProtectedRoutes'
+
+/* COMPONENTES DE ALUNO */
 import PListagemAluno from './pages/PListagem/PListagemAluno/PListagemAluno'
-import PListagemLivro from './pages/PListagem/PListagemLivro/PListagemLivro'
-import PListagemEmprestimo from './pages/PListagem/PListagemEmprestimo/PListagemEmprestimo'
 import PDetalhesAluno from './pages/PDetalhes/PDetalhesAluno/PDetalhesAluno'
 import PAtualizarAluno from './pages/PAtualizar/PAtualizarAluno/PAtualizarAluno'
+import PCadastroAluno from './pages/PCadastro/PCadastroAluno/PCadastroAluno'
+
+/* COMPONENTES DE LIVRO */
+import PListagemLivro from './pages/PListagem/PListagemLivro/PListagemLivro'
 import PDetalhesLivro from './pages/PDetalhes/PDetalhesLivro/PDetalhesLivro'
 import PAtualizarLivro from './pages/PAtualizar/PAtualizarLivro/PAtualizarLivro'
+import PCadastroLivro from './pages/PCadastro/PCadastroLivro/PCadastroLivro'
+
+/* COMPONENTES DE EMPRESTIMO */
+import PListagemEmprestimo from './pages/PListagem/PListagemEmprestimo/PListagemEmprestimo'
 import PDetalhesEmprestimo from './pages/PDetalhes/PDetalhesEmprestimo/PDetalhesEmprestimo'
 import PAtualizarEmprestimo from './pages/PAtualizar/PAtualizarEmprestimo/PAtualizarEmprestimo'
-// import ProtectedRoute from './components/Rotas/ProtectedRoutes'
+import PCadastroEmprestimo from './pages/PCadastro/PCadastroEmprestimo/PCadastroEmprestimo'
 
 function App() {
 
@@ -20,15 +29,24 @@ function App() {
       <Routes>
         <Route path='/' element={<PHome />} />
         <Route path='/login' element={<PLogin />} />
+
+        {/* ROTAS DE ALUNOS */}
         <Route path='/lista/alunos' element={<PListagemAluno />} />
-        <Route path='/lista/livros' element={<PListagemLivro />} />
-        <Route path='/lista/emprestimos' element={<PListagemEmprestimo />} />
         <Route path='/detalhes/aluno/:id_aluno' element={<PDetalhesAluno />} />
-        <Route path='/detalhes/livro/:id_livro' element={<PDetalhesLivro />} />
-        <Route path='/detalhes/emprestimo/:id_emprestimo' element={<PDetalhesEmprestimo />} />
         <Route path='/atualizar/aluno/:id_aluno' element={<PAtualizarAluno />} />
+        <Route path='/cadastro/aluno' element={<PCadastroAluno />} />
+
+        {/* ROTAS DE LIVROS */}
+        <Route path='/lista/livros' element={<PListagemLivro />} />
+        <Route path='/detalhes/livro/:id_livro' element={<PDetalhesLivro />} />
         <Route path='/atualizar/livro/:id_livro' element={<PAtualizarLivro />} />
+        <Route path='/cadastro/livro' element={<PCadastroLivro />} />
+
+        {/* ROTAS DE EMPRÉSTIMOS */}
+        <Route path='/lista/emprestimos' element={<PListagemEmprestimo />} />
+        <Route path='/detalhes/emprestimo/:id_emprestimo' element={<PDetalhesEmprestimo />} />
         <Route path='/atualizar/emprestimo/:id_emprestimo' element={<PAtualizarEmprestimo />} />
+        <Route path='/cadastro/emprestimo' element={<PCadastroEmprestimo />} />
       </Routes>
     </BrowserRouter>
   )
