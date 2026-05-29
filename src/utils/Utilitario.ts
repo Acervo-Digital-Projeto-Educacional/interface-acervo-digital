854a41cbab9b4bcb93bda8b5c49e720a0d21573c
Prof. Renato Oliveira <162147229+profRenatoOliveira1@users.noreply.github.com>
2026-05-29 14:27:38 -0300

    ajustando arquivos

--- src/utils/Utilitario.ts
+++ src/utils/Utilitario.ts
@@ -1,23 +1,23 @@
 class Utilitario {
-		// Formata a data no padrão DD/MM/AAAA
+    // Formata a data no padrão DD/MM/AAAA
     formatarData(data: string | Date): string {
         return new Date(data).toLocaleDateString("pt-br", { timeZone: "UTC" });
     }
 
-		// formata o telefone no padrão (XX) X XXXX-XXXX ou (XX) XXXX-XXXX
+    // formata o telefone no padrão (XX) X XXXX-XXXX ou (XX) XXXX-XXXX
     formatarTelefone(telefone: string): string {
         const nums = telefone.replace(/\D/g, "");  // remove qualquer caracter que não seja um número
 
-        if(nums.length === 11) {
+        if (nums.length === 11) {
             return nums.replace(/(\d{2})(\d{1})(\d{4})(\d{4})/, "($1) $2 $3-$4");
-        } else if(nums.length === 10) {
+        } else if (nums.length === 10) {
             return nums.replace(/(\d{2})(\d{4})(\d{4})/, "($1) $2-$3");
         }
 
         return telefone;
     }
 
-		// Formata moeda no valor Real BRL
+    // Formata moeda no valor Real BRL
     formatarParaReal(valor: number): string {
         return new Intl.NumberFormat('pt-br', {
             style: 'currency',
@@ -25,14 +25,14 @@ class Utilitario {
         }).format(valor);
     }
 
-		// Remove as informações de hora de um objeto Date
+    // Remove as informações de hora de um objeto Date
     formatarDataParaInput(data: string | Date | undefined): string {
-        if(!data) return '';
+        if (!data) return '';
         const d = new Date(data);
         return d.toISOString().split('T')[0];
     }
 
-		// Valida um e-mail através de uma expressão regular
+    // Valida um e-mail através de uma expressão regular
     validarEmail(email: string) {
         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return regex.test(email);
